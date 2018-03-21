801fb635e69d4626530f647bf3061b3bba6289a8
Jonas Augusto <jonas.augusto@ddsnet.dds.com.br>
2018-03-21 15:48:05 -0300

    Criado accordion de atributos

--- src/app/ficha/ficha.module.ts
+++ src/app/ficha/ficha.module.ts
@@ -7,10 +7,12 @@ import { SubatributosComponent } from './subatributos/subatributos.component';
 import { CombateComponent } from './combate/combate.component';
 import { DeslocamentoComponent } from './deslocamento/deslocamento.component';
 import { MatInputModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
-  imports: [CommonModule, MatInputModule, BrowserAnimationsModule],
+  imports: [CommonModule, MatInputModule, MatGridListModule, BrowserAnimationsModule, MatExpansionModule],
   declarations: [IdentidadeComponent, AtributosComponent, EnergiasComponent, SubatributosComponent, CombateComponent, DeslocamentoComponent],
   exports: [IdentidadeComponent, AtributosComponent, EnergiasComponent, SubatributosComponent, CombateComponent, DeslocamentoComponent]
 })
