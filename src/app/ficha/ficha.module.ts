3881bcd35bcae6919e913ef81d4b9a33287dc22d
Jonas Augusto <jonas.augusto@ddsnet.dds.com.br>
2018-03-21 17:53:31 -0300

    Inicio de desenvolvimento do componente de energia

--- src/app/ficha/ficha.module.ts
+++ src/app/ficha/ficha.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { IdentidadeComponent } from './identidade/identidade.component';
 import { AtributosComponent } from './atributos/atributos.component';
 import { EnergiasComponent } from './energias/energias.component';
 import { SubatributosComponent } from './subatributos/subatributos.component';
 import { CombateComponent } from './combate/combate.component';
 import { DeslocamentoComponent } from './deslocamento/deslocamento.component';
+
 import { MatInputModule } from '@angular/material';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, MatInputModule, MatGridListModule, BrowserAnimationsModule, MatExpansionModule],
+  imports: [CommonModule, MatInputModule, MatGridListModule, BrowserAnimationsModule, MatExpansionModule, MatProgressBarModule, MatCardModule, MatButtonModule],
   declarations: [IdentidadeComponent, AtributosComponent, EnergiasComponent, SubatributosComponent, CombateComponent, DeslocamentoComponent],
   exports: [IdentidadeComponent, AtributosComponent, EnergiasComponent, SubatributosComponent, CombateComponent, DeslocamentoComponent]
 })
