13eae871614dc5bbbd84f9d997fe32378f120da4
Jonas Augusto <jonas.augusto@ddsnet.dds.com.br>
2018-03-27 17:38:02 -0300

    Criado serviço de geração de Destreza com testes

--- src/app/shared/services/geradores/gera-destreza.service.spec.ts
+++ src/app/shared/services/geradores/gera-destreza.service.spec.ts
@@ -3,13 +3,24 @@ import { TestBed, inject } from '@angular/core/testing';
 import { GeraDestrezaService } from './gera-destreza.service';
 
 describe('GeraDestrezaService', () => {
+  let service: GeraDestrezaService;
+  let seed: number;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [GeraDestrezaService]
     });
+    this.service = new GeraDestrezaService();
+    this.seed = Math.random();
   });
 
   it('should be created', inject([GeraDestrezaService], (service: GeraDestrezaService) => {
     expect(service).toBeTruthy();
   }));
+
+  it ('gera uma Destreza válida', () =>{
+    var result = this.service.get(this.seed);
+    console.log(result);
+    expect(result).toBeDefined();
+  })
 });
--- src/app/shared/services/geradores/gera-destreza.service.ts
+++ src/app/shared/services/geradores/gera-destreza.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Gerador } from './gerador';
+import { GeraInteiroService } from './gera-inteiro.service';
+import { GeraBooleanService } from './gera-boolean.service';
+import { GeraStringService } from './gera-string.service';
+import { GeraValorMagService } from './gera-valor-mag.service';
+import { Destreza } from '../../classes/atributos/destreza';
+import { ValorMag } from '../..';
 
 @Injectable()
-export class GeraDestrezaService {
-
+export class GeraDestrezaService implements Gerador {
+  private rng: GeraInteiroService;
+  private rbg: GeraBooleanService;
+  private rsg: GeraStringService;
+  private rvmg: GeraValorMagService;
+  
+  get(seed: number): Destreza {
+    var result = new Destreza();
+    this.rng = new GeraInteiroService();
+    this.rsg = new GeraStringService();
+    this.rvmg = new GeraValorMagService();
+    var min = new ValorMag(10, 1);
+    var max = new ValorMag(99, 15);
+    result.ataque = this.rvmg.getEntre(Math.random(), min, max);
+    result.bonusCP = this.rng.getEntre(Math.random(), 1, 9999);
+    result.classe = this.rsg.getTamanhoEspecifico(Math.random(), 1,1);
+    result.coordenacao = this.rvmg.getEntre(Math.random(), min, max);
+    result.esquiva = this.rvmg.getEntre(Math.random(), min, max);
+    result.iniciativa = this.rvmg.getEntre(Math.random(), min, max);
+    result.nivel = this.rng.getEntre(Math.random(), 1, 5);
+    result.pontos = this.rng.getEntre(Math.random(), 1, 9999);
+    result.porcentagem = this.rvmg.getEntre(Math.random(), min, max);
+    result.reflexo = this.rvmg.getEntre(Math.random(), min, max);
+    return result;
+  }
+  getLista(seed: number, quantidade: number): Destreza[] {
+    var resultado = [];
+    for (let i = 0; i < quantidade; i++){
+      resultado.push(this.get(seed));
+    }
+    return resultado; 
+  }
   constructor() { }
 
 }
