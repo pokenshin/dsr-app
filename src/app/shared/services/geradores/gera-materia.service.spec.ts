26d1f7b4d37a67aeef61a83d5c550977d99b4bd4
Jonas Augusto <jonas.augusto@ddsnet.dds.com.br>
2018-03-27 17:29:12 -0300

    Criado gerador de Materia com testes

--- src/app/shared/services/geradores/gera-materia.service.spec.ts
+++ src/app/shared/services/geradores/gera-materia.service.spec.ts
@@ -3,13 +3,24 @@ import { TestBed, inject } from '@angular/core/testing';
 import { GeraMateriaService } from './gera-materia.service';
 
 describe('GeraMateriaService', () => {
+  let service: GeraMateriaService;
+  let seed: number;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [GeraMateriaService]
     });
+    this.service = new GeraMateriaService();
+    this.seed = Math.random();
   });
 
   it('should be created', inject([GeraMateriaService], (service: GeraMateriaService) => {
-    expect(service).toBeTruthy();
+    expect(this.service).toBeTruthy();
   }));
+
+  it('deve gerar uma Materia válida', () =>{
+    var result = this.service.get(this.seed);
+    console.log(result);
+    expect(result).toBeDefined();
+  })
 });
--- src/app/shared/services/geradores/gera-materia.service.ts
+++ src/app/shared/services/geradores/gera-materia.service.ts
@@ -1,7 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Gerador } from './gerador';
+import { GeraInteiroService } from './gera-inteiro.service';
+import { GeraBooleanService } from './gera-boolean.service';
+import { GeraStringService } from './gera-string.service';
+import { GeraValorMagService } from './gera-valor-mag.service';
+import { Materia } from '../../classes/atributos/materia';
+import { ValorMag } from '../..';
 
 @Injectable()
-export class GeraMateriaService {
+export class GeraMateriaService implements Gerador {
+  private rng: GeraInteiroService;
+  private rbg: GeraBooleanService;
+  private rsg: GeraStringService;
+  private rvmg: GeraValorMagService;
+  
+  get(seed: number):Materia {
+    var result = new Materia();
+    this.rng = new GeraInteiroService();
+    this.rsg = new GeraStringService();
+    this.rvmg = new GeraValorMagService();
+    var min = new ValorMag(10, 1);
+    var max = new ValorMag(99, 15);
+    
+    result.bonusCP = this.rng.getEntre(Math.random(), 1, 9999);
+    result.bonusHP = this.rvmg.getEntre(Math.random(), min, max);
+    result.classe = this.rsg.getTamanhoEspecifico(Math.random(), 1,1);
+    result.colapso = this.rvmg.getEntre(Math.random(), min, max);
+    result.impulso = this.rvmg.getEntre(Math.random(), min, max);
+    result.nivel = this.rng.getEntre(Math.random(), 1, 5);
+    result.pontos = this.rng.getEntre(Math.random(), 1, 9999);
+    result.porcentagem = this.rvmg.getEntre(Math.random(), min, max);
+    result.regeneracao = this.rvmg.getEntre(Math.random(), min, max);
+    result.resistencia = this.rvmg.getEntre(Math.random(), min, max);
+    result.vitalidade = this.rvmg.getEntre(Math.random(), min, max);
+    
+    return result;
+  }
+  getLista(seed: number, quantidade: number): any[] {
+    var resultado = [];
+    for (let i = 0; i < quantidade; i++){
+      resultado.push(this.get(seed));
+    }
+    return resultado; 
+  }
 
   constructor() { }
 
