9a576f76337c30196fd96f89b0ece6cf902087f2
Jonas Augusto <jonas.augusto@ddsnet.dds.com.br>
2018-04-16 12:30:05 -0300

    Componente de Energias criado

--- src/app/ficha/energias/energias.component.ts
+++ src/app/ficha/energias/energias.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Energia } from 'shared/core/energias';
+import {MatTableDataSource} from '@angular/material';
+
 
 @Component({
   selector: 'app-ficha-energias',
@@ -6,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./energias.component.css']
 })
 export class EnergiasComponent implements OnInit {
+  @Input() energias:Energia[];
+  columnsToDisplay = ['sigla', 'quantidade'];
+  dataSource = new MatTableDataSource(this.energias);
 
   constructor() { }
 
--- src/app/ficha/view-ficha/view-ficha.component.ts
+++ src/app/ficha/view-ficha/view-ficha.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Ser, Identidade } from 'shared/core/ser';
 import { GeraSerService } from 'shared/services/geradores/seres';
 import { AtributosSer } from 'shared/core/ser/atributos';
+import { Energia } from 'shared/core/energias';
 
 @Component({
   selector: 'app-view-ficha',
@@ -12,6 +13,7 @@ export class ViewFichaComponent implements OnInit {
   ser:Ser;
   identidade:Identidade;
   atributos:AtributosSer;
+  energias:Energia[];
 
   constructor() { }
 
@@ -20,6 +22,7 @@ export class ViewFichaComponent implements OnInit {
     this.ser = gSer.get(Math.random());
     this.identidade = this.ser.identidade;
     this.atributos = this.ser.atributos;
+    this.energias = this.ser.energias;
   }
 
 }
